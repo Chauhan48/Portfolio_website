714553a0f5e7bd1f7713c13713dffeb11ff96a96
Anshit Chauhan <chauhananshit48@gmail.com>
2025-09-03 11:48:51 +0530

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+const Navbar = () => {
+  return (
+    <div>
+      <nav>
+        <div>
+            <a>Education</a>
+            <a>Projects</a>
+            <a>Work Experience</a>
+        </div>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar
